c974f97b852ba6ba1752312b03f8b7796f7c58cc
sharath <2sharathkumar.b@gmail.com>
2024-08-31 10:58:20 +0530

    initial change for dummy release

---
+++ first.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<unordered_map>
+#include<string>
+using namespace std;
+int main(){
+    string word1 = "tokniyexicuhj";
+    string word2 = "tution";
+    unordered_map<char, int> mp;
+    for(int i=0;i<word1.size(); i++){
+        mp[word1[i]]++;
+    }
+    int exist = 0;
+    int n = word1.length(), m = word2.length();
+    for(int j=0;j<m;j++){
+        if(mp.find(word2[j])!=mp.end()){
+            mp[word2[j]]--;
+            if(mp[word2[j]]==0)mp.erase(word2[j]);
+            exist++;
+        }
+    }
+    cout<<n-exist;
+    return 0;
+}
